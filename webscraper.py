9b5899e93c72c462005b2b9aead33a2ea8276bd6
Owen JP <owenjp311@gmail.com>
2023-03-29 11:13:08 +0100

    Add extract_url and extract_link_index functions to produce the url list output

--- webscraper.py
+++ webscraper.py
@@ -17,7 +17,27 @@ def get_content(url):
     content_string = article_list[0]
     article_index = content_string.index("itemListElement")
     content_string = content_string[article_index + 18:]
+    return content_string
     
 
-get_content("https://www.nytimes.com/section/technology")
+def extract_link_index(content_str, current_year):
+    start_index = []
+    end_index = []
+    for i in range(len(content_str)):
+        if content_str.startswith(f"https://www.nytimes.com/{current_year}", i):
+            start_index.append(i)
+        if content_str.startswith("html", i):
+            end_index.append(i + 4)
+    return start_index, end_index
+
+
+def extract_url(link_indexs, cotent_string):
+    url_list = []
+    for i in range(len(link_indexs[0])):
+        url_list.append(content_string[link_indexs[0][i]:link_indexs[1][i]])
+    return url_list
+
+content_string = get_content("https://www.nytimes.com/section/technology")
+link_indexs = extract_link_index(content_string, 2023)
+aricle_urls = extract_url(link_indexs, content_string)
 
